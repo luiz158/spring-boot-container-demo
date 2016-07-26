8f0e45596b9f1f6aa85cd5a246eb922ef7c4279a
omerio <omer.dawelbeit@gmail.com>
2016-07-26 20:34:35 +0100

    The GAEConfig wasn't working

--- src/main/java/com/google/example/GAEConfig.java
+++ src/main/java/com/google/example/GAEConfig.java
@@ -19,15 +19,17 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 
 @Profile("GAE")
+@PropertySource(value = {"classpath:/config/application-gae.properties", "classpath:/config/application.properties"})
 @Configuration
 public class GAEConfig {
     @Value("${spring.redis.host}")
     private String redisHost;
 
-    @Value("spring.redis.port}")
+    @Value("${spring.redis.port}")
     private Integer redisPort;
 
     @Bean
